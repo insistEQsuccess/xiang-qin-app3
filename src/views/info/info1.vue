2615e632c2c168e0ded07508a00ee13f8ab43654
insistEQsuccess <1943952626@qq.com>
2022-09-29 22:57:51 +0800

    aa

--- src/views/info/info1.vue
+++ src/views/info/info1.vue
@@ -15,7 +15,7 @@
     </div>
     <div class="info-item">
       <div class="item-l">您的自身条件</div>
-      <div class="item-r">
+      <div class="item-r" @click="goSelfInfo">
         <div class="r-txt">去完善</div>
         <svg t="1664288770307" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2534" width="200" height="200"><path d="M857.70558 495.009024 397.943314 27.513634c-7.132444-7.251148-18.794042-7.350408-26.048259-0.216941-7.253194 7.132444-7.350408 18.795065-0.216941 26.048259l446.952518 454.470749L365.856525 960.591855c-7.192819 7.192819-7.192819 18.85544 0 26.048259 3.596921 3.596921 8.311293 5.39487 13.024641 5.39487s9.42772-1.798972 13.024641-5.39487L857.596086 520.949836C864.747973 513.797949 864.796068 502.219239 857.70558 495.009024z" p-id="2535" fill="#8a8a8a"></path></svg>
       </div>
@@ -63,10 +63,12 @@
 </template>
 <script lang="ts">
 import { defineComponent, reactive } from 'vue'
+import { useRouter } from 'vue-router'
 import { Dialog } from 'vant';
 export default defineComponent({
   name: 'Info1',
   setup () {
+    const $router = useRouter()
     const formData = reactive({ isPublic: 0 })
     const imgList = reactive([])
     function onPublicChange (e: any) {
@@ -83,9 +85,13 @@ export default defineComponent({
         });
       }
     }
+    function goSelfInfo () {
+      $router.push('/info')
+    }
     return {
       formData,
       imgList,
+      goSelfInfo,
       onPublicChange
     }
   }
