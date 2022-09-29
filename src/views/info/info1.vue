576313e45ea55f26ce84449282824266102a6200
insistEQsuccess <1943952626@qq.com>
2022-09-29 22:55:51 +0800

    info1

--- src/views/info/info1.vue
+++ src/views/info/info1.vue
@@ -28,21 +28,66 @@
       </div>
     </div>
     <div class="info-item">
+      <div class="item-l">公开联系方式</div>
+      <div class="item-r">
+        <div class="public-box">
+          <label><input type="radio" name="isPublic" :value="1" v-model="formData.isPublic" @change="onPublicChange"/>是</label>
+          <label><input type="radio" name="isPublic" :value="0" v-model="formData.isPublic" @change="onPublicChange"/>否</label>
+        </div>
+      </div>
+    </div>
+    <div class="info-item">
       <div class="item-h">
         <div class="h-txt">附1-3张生活照（选填）</div>
-        <div class="img1-box">
-          <div class="img1-up"></div>
+        <div class="img-wraper">
+          <div v-show="imgList.length">
+            <div class="img1-box" v-for="(it, index) in imgList" :key="index">
+              <img :src="it" alt="">
+            </div>
+          </div>
+          <div class="img1-box" v-show="!imgList.length">
+            <svg t="1664461634454" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2127" width="200" height="200"><path d="M877.4 528.2H148.7c-10.5 0-19.1-8.6-19.1-19.1s8.6-19.1 19.1-19.1h728.7c10.5 0 19.1 8.6 19.1 19.1s-8.5 19.1-19.1 19.1z" fill="#333333" p-id="2128"></path><path d="M513.1 892.5c-10.5 0-19.1-8.6-19.1-19.1V144.7c0-10.5 8.6-19.1 19.1-19.1 10.5 0 19.1 8.6 19.1 19.1v728.7c0 10.6-8.6 19.1-19.1 19.1z" fill="#333333" p-id="2129"></path></svg>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="info-item">
+      <div class="item-h">
+        <div class="h-txt">附1-3张生活照（选填）</div>
+        <div class="img-wraper">
+          <textarea cols="30" rows="10" class="normal-txt" placeholder="选填，例如您的优势或特殊说明"></textarea>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
+import { Dialog } from 'vant';
 export default defineComponent({
   name: 'Info1',
   setup () {
-    return {}
+    const formData = reactive({ isPublic: 0 })
+    const imgList = reactive([])
+    function onPublicChange (e: any) {
+      if (e.target.value == 1) {
+        Dialog.confirm({
+          title: '温馨提示',
+          message:
+            '您已选择公开联系方式，他人将直接获取您的联系方式，是否确认公开？',
+        }).then(() => {
+          console.log('on confirm')
+        }).catch(() => {
+          console.log('on cancel')
+          formData.isPublic = 0
+        });
+      }
+    }
+    return {
+      formData,
+      imgList,
+      onPublicChange
+    }
   }
 })
 </script>
@@ -84,6 +129,43 @@ export default defineComponent({
       .r-txt{
         font-size: 16px;
         color: #ccc;
+      }
+      .public-box{
+        display: flex;
+        justify-content: space-between;
+        width: 200px;
+        padding-right: 20px;
+        box-sizing: border-box;
+      }
+    }
+    .item-h{
+      width: 100%;
+      .img-wraper{
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        .img1-box{
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 70px;
+          height: 70px;
+          margin-top: 10px;
+          background-color: #e8e8e8;
+          svg{
+            width: 50px;
+            height: 50px;
+          }
+        }
+        .normal-txt{
+          height: 80px;
+          width: 100%;
+          margin-top: 10px;
+          padding: 10px;
+          resize: both;
+          outline: none;
+          border: 1px solid #e8e8e8;
+        }
       }
     }
   }
---
+++ src/views/info/update-avatar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="avatar-box">
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'UpdateAvatar',
+  setup () {
+    return {}
+  }
+})
+</script>
