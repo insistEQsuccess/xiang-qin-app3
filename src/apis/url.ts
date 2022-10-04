5115f2acf684327fc4ec9f1f068d6da385bae7cc
insistEQsuccess <1943952626@qq.com>
2022-10-04 14:55:20 +0800

    login1

--- src/apis/url.ts
+++ src/apis/url.ts
@@ -4,3 +4,5 @@ import { post } from './https-service'
 export const getList = (data: any) => post('/blindDateWall/visitingCard/list', data)
 // 上传文件
 export const uploadImg = (data: any) => post('/user/icon/upload', data)
+// login
+export const loginFun = (data: any) => post('/user/login', data)
