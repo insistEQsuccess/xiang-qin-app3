294e7679e2e7b7caed3ac45225700d5fd2cf1335
insistEQsuccess <1943952626@qq.com>
2022-10-08 23:20:01 +0800

    11

--- src/apis/url.ts
+++ src/apis/url.ts
@@ -1,17 +1,19 @@
 import { post } from './https-service'
 import { encrypt, decropt } from '@/util/index'
 const enStr = encrypt()
-console.log('查看解密结果：---')
-console.log(decropt(enStr))
+console.log('看看加密结果-----')
+console.log(enStr)
+// console.log('查看解密结果：---')
+// console.log(decropt(enStr))
 
 // 列表接口
 export const getList = (data: any) => post('/blindDateWall/visitingCard/list', data, { sign: encrypt() })
 // 上传文件
 export const uploadImg = (data: any) => post('/user/icon/upload', data, { sign: encrypt() })
 // login
-export const loginFun = (data: any) => post('/user/login', data, { sign: encrypt() })
+export const loginFun = (data: any, config: any) => post('/user/login', data, config)
 // 获取验证码
-export const getCodeFun = (data: any) => post('/sms/send', data, { sign: encrypt() })
+export const getCodeFun = (data: any, config: any) => post('/sms/send', data, config)
 // 注册用户
 export const registerUser = (data: any) => post('/user/register', data, { sign: encrypt() })
 // 用户详情
--- src/util/index.ts
+++ src/util/index.ts
@@ -5,16 +5,24 @@ export const MIYAO = "q6w6oQ1Jz0V82jZ9";//AES 加密
 const iv = "rb!nBwXv4C%Gr^84";
 // 加密
 export function encrypt(){
-  const param: any = { timestamp: Date.now(), os: 'h5' }
-  const user_id = localStorage.getItem('user_id')
-  if (user_id) {
-    param.userId = user_id
+  // const param: any = { timestamp: Date.now(), os: 'h5', token: '', userId: '' }
+  const param: any = {"userId":"","os":"Iphone","imei":"c9a13587821f491685b75e9d1b83aaef","model":"iPhone Simulator","version":10,"token":"","timestamp": Date.now()}
+  const userId = localStorage.getItem('userId')
+  const token = localStorage.getItem('token')
+  if (userId) {
+    param.userId = userId
   }
-  return CryptoJS.AES.encrypt(JSON.stringify(param), MIYAO, {
+  if (token) {
+    param.token = token
+  }
+  let ret = CryptoJS.AES.encrypt(JSON.stringify(param), MIYAO, {
     iv: CryptoJS.enc.Utf8.parse(iv),
-    mode: CryptoJS.mode.CFB,
-    padding: CryptoJS.pad.AnsiX923
+    // mode: CryptoJS.mode.CBC,
+    // padding: CryptoJS.pad.Pkcs7
   }).toString();
+  // ret = window.btoa(ret)
+  // return window.btoa(ret)
+  return ret
 }
 // 解密
 export function decropt (enStr: any) {
